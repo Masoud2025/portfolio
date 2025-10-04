1b3976eb970e6c6e6d30134ec10c353867621083
Masoud2025 <Masoud.jafary.@outlook.com>
2025-10-04 18:26:12 +0330

    feat(ui): Add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/layout/head/Navbar";
+import Hero from "@/components/ui/Hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/ui/Hero.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { JSX } from "react";
+
+export default function Hero(): JSX.Element {
+  return (
+    <section className="flex justify-center items-center mt-56">
+      <h1 className="text-[52px] text-gray-500">مسعود جعفری</h1>
+    </section>
+  );
+}
