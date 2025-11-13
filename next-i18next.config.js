b6c1c98e729365cd36daa502ae6c7dd6959fbc1b
Masoud2025 <Masoud.jafary.@outlook.com>
2025-11-13 17:16:14 +0330

    feat(next-i18next.): +Create configuration file

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-i18next').NextI18NextConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: "fa",
+    locales: ["fa", "en"],
+  },
+  react: { useSuspense: false },
+};
