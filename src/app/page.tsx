4b7aeb01816f89167fde0418a8434969a9397166
Masoud2025 <Masoud.jafary.@outlook.com>
2025-11-22 21:46:43 +0330

    remove old design work on new one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import MainContent from "@/components/layout/MainContent";
-import SideBar from "@/components/layout/Sidebar/MainSidebar";
-import { ThemeProvider } from "@/components/ThemeContext";
-
 // import ResponsiveSimulator from "@/components/ui/ResponsiveSimulator";
+
+import NewHero from "../components/NewHero";
 
 export default function Home() {
   return (
     <div>
-      <ThemeProvider>
-        <div className="w-full min-h-screen md:grid md:grid-cols-[260px_auto] gap-6  md:px-36 pt-6">
-          <SideBar />
-          <MainContent />
-        </div>
-      </ThemeProvider>
+      <div className="">
+        <NewHero/>
+      </div>
     </div>
   );
 }
---
+++ src/components/NewHero.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewHero() {
+  return (
+    <div>NewHero</div>
+  )
+}
+
+export default NewHero
