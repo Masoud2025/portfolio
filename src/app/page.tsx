9b3f02a08c6fd2f429720d9eac921b9211b10b71
Masoud2025 <Masoud.jafary.@outlook.com>
2025-12-21 22:18:05 +0330

    3D Portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,22 @@
 // import ResponsiveSimulator from "@/components/ui/ResponsiveSimulator";
 
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Hero from "../components/Heero";
-import TopBar from "../components/Navbar";
-import NaviBar from "../components/NaviBar";
-import Projects from "../components/Projectss";
+import FullCubePage from "../components/3d-cube-hero";
 
 export default function Home() {
   return (
     <div>
       <div className="w-screen h-screen ">
-        <TopBar></TopBar>
-        <NaviBar></NaviBar>
-        <Hero></Hero>
-        <Projects />
-        <Contact/>
-        <Footer />
+        {/* <TopBar></TopBar> */}
+        {/* <NaviBar></NaviBar> */}
+        {/* <Hero></Hero> */}
+        {/* <Projects /> */}
+        {/* <Contact/> */}
+        {/* <Footer /> */}
         {/* <Hero></Hero> */}
         {/* <ItGuy></ItGuy> */}
         {/* <AuroraBackground></AuroraBackground> */}
         {/* <CubeHero></CubeHero> */}
+        <FullCubePage />
       </div>
     </div>
   );
